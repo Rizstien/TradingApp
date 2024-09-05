63a35682419aa017d6bd618db2e836e9688e18cf
usmanahmed737 <usman.ahmed@ebitlogix.com>
2024-09-05 11:13:48 +0500

    initial commits

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,13 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./shared/component/header/header.component";
 import { FooterComponent } from "./shared/component/footer/footer.component";
+import { DashboardComponent } from './shared/component/dashboard/dashboard.component';
+import { TradingComponent } from './components/TradingListView/trading/trading.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, FooterComponent],
+  imports: [RouterOutlet, TradingComponent, HeaderComponent, FooterComponent, DashboardComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- src/app/shared/component/dashboard/dashboard.component.ts
+++ src/app/shared/component/dashboard/dashboard.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { CoinService } from '../../../core/services/coin.service';
 import { CommonModule } from '@angular/common';
+
  
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.scss'
 })
