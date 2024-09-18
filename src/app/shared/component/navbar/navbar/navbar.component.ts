b1edb969e8e7286e4c002c809d60babd820be06f
Huzaifa_1 <huzaifaali1135@gmail.com>
2024-09-18 18:53:01 +0500

    add logout and redirect to login page

--- src/app/shared/component/navbar/navbar/navbar.component.ts
+++ src/app/shared/component/navbar/navbar/navbar.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import { Router, RouterModule } from '@angular/router';
+import { User } from '../../../../models/user.model';
 
 @Component({
   selector: 'app-navbar',
@@ -9,5 +10,14 @@ import { RouterModule } from '@angular/router';
   styleUrl: './navbar.component.scss'
 })
 export class NavbarComponent {
+
+  user: User = new User;
+  constructor(
+    public route: Router,
+  ) {}
+
+  signOut(){
+    this.route.navigate(['/login']);
+  }
 
 }
