f96f10896945fd2815c0aa9d1af0f51a8b7aa548
usmanahmed737 <usman.ahmed@ebitlogix.com>
2024-09-19 22:52:47 +0500

    structural changes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,7 +2,7 @@ import { Routes } from '@angular/router';
 import { TradingComponent } from './components/TradingListView/trading/trading.component';
 import { LoginComponent } from './components/login/login.component';
 import { SignUpComponent } from './components/sign-up/sign-up.component';
-import { authGuard } from './shared/auth/auth.guard';
+import { authGuard } from './core/interceptors/guards/auth.guard';
 
 export const routes: Routes = [
 {path: '',redirectTo: 'login',pathMatch: 'full'},
