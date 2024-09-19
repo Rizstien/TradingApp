62d4e40aa1b318bfc21f451e4c664bd8ca6e9f4b
usmanahmed737 <usman.ahmed@ebitlogix.com>
2024-09-20 02:19:54 +0500

    trading list view completed

--- src/app/components/listviews/coins/trades/trade/trade.component.ts
+++ src/app/components/listviews/coins/trades/trade/trade.component.ts
@@ -1,50 +1,34 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
-import { environment } from '../../../../../../environments/environment';
-import { T } from '@angular/cdk/keycodes';
-import { JsonPipe } from '@angular/common';
+import { CommonModule, JsonPipe } from '@angular/common';
+import { TradingService } from '../../../../../shared/service/trade/trading.service';
+import { NavbarComponent } from '../../../../../shared/component/navbar/navbar/navbar.component';
+
+
+
 
 @Component({
   selector: 'app-trade',
   standalone: true,
-  imports: [JsonPipe],
+  imports: [CommonModule, JsonPipe, NavbarComponent],
   templateUrl: './trade.component.html',
   styleUrl: './trade.component.scss'
 })
 export class TradeComponent  implements OnInit {
-    tradeList : any;
-    constructor(public httpClient: HttpClient)
-    {
-       
-    }
-    
+    tradeList: any[] = [];
+    constructor(public httpClient: HttpClient,private tradingservice: TradingService){ }
     ngOnInit(): void {
         console.log('ngOnInit called');
         this.getMockAPITradeList();
     }
-    getMockAPITradeList(){
-      this.httpClient.get("https://api.apicalls.io/v1/markets/insider-trades").subscribe((res : any) => {
-        this.tradeList = res;
-      })
-
+    getMockAPITradeList(){    
+      this.tradingservice.fetchData('https://api.apicalls.io/v1/markets/insider-trades').subscribe((data) => {
+          console.log(data);
+          this.tradeList = data;
+        },
+        (error) => {
+          console.error('Error fetching data:', error);
+        }
+      );
     }
-
-
-    // getMockAPITradeList(){
-      
-    
-    
-    
-    //   this.tradingService.fetchData('https://api.apicalls.io/v1/markets/insider-trades').subscribe((data) => {
-    //       this.tradeList = data; // Data contains the 'body' array
-    //     },
-    //     (error) => {
-    //       console.error('Error fetching data:', error);
-    //     }
-    //   );
-    
-    
-    
-    
-    // }
 }
